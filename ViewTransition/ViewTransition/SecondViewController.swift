77ea22fc5ca0c689b3e47832532ea9c7eab3e6e7
sieuns <mn07020@gmail.com>
2021-08-26 17:05:08 +0900

    뷰의 상태변화 감지 메서드

--- ViewTransition/ViewTransition/SecondViewController.swift
+++ ViewTransition/ViewTransition/SecondViewController.swift
@@ -13,6 +13,43 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("SecondViewController의 view가 메모리에 로드 됨")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("SecondViewController의 view가 화면에 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("SecondViewController의 view가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("SecondViewController의 view가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("SecondViewController의 view가 화면에서 사라짐")
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("SecondViewController의 view가 subview를 레이아웃 하려함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("SecondViewController의 view가 subview를 레이아웃 함")
     }
     
     @IBAction func popToprev(){
